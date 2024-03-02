c1aafd2a97f885a7f17c0cf2b19ee5e19a85a802
yash chokhani <yashchokhani95@gmail.com>
2024-03-02 19:32:30 +0530

    update: User login and signup

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -4,55 +4,74 @@ const {
   AdminInitiateAuthCommand,
   CognitoIdentityProviderClient,
 } = require("@aws-sdk/client-cognito-identity-provider");
+
 const client = new CognitoIdentityProviderClient({});
+
 const { addUser, addData } = require("./dynamodbControllers");
-// addData();
-// addUser("test", "yash.com");
+
 const signUp = async ({ username, password, email }) => {
-  const command = new AdminCreateUserCommand({
-    UserPoolId: process.env.COGNITO_USER_POOL_ID,
-    Username: username,
-    UserAttributes: [
-      { Name: "email", Value: email },
-      {
-        Name: "email_verified",
-        Value: "true",
+  try {
+    const command = new AdminCreateUserCommand({
+      UserPoolId: process.env.COGNITO_USER_POOL_ID,
+      Username: username,
+      UserAttributes: [
+        { Name: "email", Value: email },
+        {
+          Name: "email_verified",
+          Value: "true",
+        },
+      ],
+      MessageAction: "SUPPRESS",
+    });
+    const user = await client.send(command);
+    if (!user) {
+      throw new Error("User not created");
+    }
+    const passwordCommand = new AdminSetUserPasswordCommand({
+      UserPoolId: process.env.COGNITO_USER_POOL_ID,
+      Username: username,
+      Password: password,
+      Permanent: true,
+    });
+    const passwordResponse = await client.send(passwordCommand);
+    const userId = await addUser({ username, email });
+    return {
+      success: true,
+      message: "User created successfully",
+      data: {
+        userId,
       },
-    ],
-    MessageAction: "SUPPRESS",
-  });
-  const user = await client.send(command);
-  if (!user) {
-    throw new Error("User not created");
+    };
+  } catch (err) {
+    throw err;
   }
-  const passwordCommand = new AdminSetUserPasswordCommand({
-    UserPoolId: process.env.COGNITO_USER_POOL_ID,
-    Username: username,
-    Password: password,
-    Permanent: true,
-  });
-  const passwordResponse = await client.send(passwordCommand);
-  return {
-    user,
-    passwordResponse,
-  };
 };
 
 const login = async ({ username, password }) => {
-  const user_pool_id = process.env.COGNITO_USER_POOL_ID;
-  const client_id = process.env.COGNITO_USER_POOL_CLIENT_ID;
-  const command = new AdminInitiateAuthCommand({
-    AuthFlow: "ADMIN_USER_PASSWORD_AUTH",
-    UserPoolId: user_pool_id,
-    ClientId: client_id,
-    AuthParameters: {
-      USERNAME: username,
-      PASSWORD: password,
-    },
-  });
-  const response = await client.send(command);
-  console.log(response);
-  return response;
+  try {
+    const user_pool_id = process.env.COGNITO_USER_POOL_ID;
+    const client_id = process.env.COGNITO_USER_POOL_CLIENT_ID;
+    const command = new AdminInitiateAuthCommand({
+      AuthFlow: "ADMIN_USER_PASSWORD_AUTH",
+      UserPoolId: user_pool_id,
+      ClientId: client_id,
+      AuthParameters: {
+        USERNAME: username,
+        PASSWORD: password,
+      },
+    });
+    const response = await client.send(command);
+    console.log(response);
+    return {
+      success: true,
+      message: "User logged in successfully",
+      data: {
+        Authentication: response.AuthenticationResult,
+      },
+    };
+  } catch (err) {
+    throw err;
+  }
 };
 
 module.exports = {
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,13 +6,28 @@ router.get("/", (req, res) => {
 });
 router.post("/signup", async (req, res) => {
   const { username, password, email } = req.body;
-  const response = await signUp({ username, password, email });
-  res.send(response);
+  try {
+    const response = await signUp({ username, password, email });
+    res.status(201).send(response); // 201 status code for resource created
+  } catch (error) {
+    console.error("Error in signUp:", error);
+    res
+      .status(500)
+      .send({ success: false, message: "Internal Server Error", data: {} }); // 500 status code for internal server error
+  }
 });
+
 router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  const response = await login({ username, password });
-  res.send(response);
+  try {
+    const { username, password } = req.body;
+    const response = await login({ username, password });
+    res.status(200).send(response); // 200 status code for success
+  } catch (err) {
+    console.error("Error in login:", err);
+    res
+      .status(500)
+      .send({ success: false, message: "Internal Server Error", data: {} });
+  }
 });
 
 module.exports = router;
