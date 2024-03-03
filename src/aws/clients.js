03377182239ab2de174e50aad02ef128d678d3ea
yash chokhani <yashchokhani95@gmail.com>
2024-03-04 00:41:59 +0530

    feat: aws sdk clients added

---
+++ src/aws/clients.js
@@ -0,0 +1,28 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  CognitoIdentityProviderClient,
+} = require("@aws-sdk/client-cognito-identity-provider");
+const { S3Client } = require("@aws-sdk/client-s3");
+const dynamodbClient = new DynamoDBClient({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.MY_APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.MY_APP_AWS_SECRET_ACCESS_KEY,
+  },
+});
+const cognitoClient = new CognitoIdentityProviderClient({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.MY_APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.MY_APP_AWS_SECRET_ACCESS_KEY,
+  },
+});
+const s3Client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+module.exports = { dynamodbClient, cognitoClient, s3Client };
